b736e7914874d187ab3628154a1e5c1b9cf14380
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-09-26 06:11:25 +0000

    Update 2 files

--- file.js
+++ file.js
@@ -1,2 +1,4 @@
 const fs = require("fs");
-fs.writeFile("./test.txt", "Hello World Async", (err)=>{})
+fs.writeFile("./test.txt", "Hello World Async", (err) => {});
+const result = fs.readFileSync("./c.txt", "utf-8");
+console.log(result);
