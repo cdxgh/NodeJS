7102a4fd66646b053318565380dfbc6f05bf0cd3
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-09-26 06:42:58 +0000

    Update file.js and test.txt

--- file.js
+++ file.js
@@ -1,4 +1,13 @@
 const fs = require("fs");
-fs.writeFile("./test.txt", "Hello World Async", (err) => {});
-const result = fs.readFileSync("./c.txt", "utf-8");
-console.log(result);
+// fs.writeFile("./test.txt", "Hello World Async", (err) => {});
+// const result = fs.readFileSync("./c.txt", "utf-8");
+// console.log(result);
+fs.readFile("./c.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("Error",err);
+    }
+    else{
+        console.log(result);
+    } 
+});
+fs.appendFileSync("./test.txt", `Hey there\n`);
